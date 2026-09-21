89941d86a8a89639d6cc38db750261359866ff49
Mehradt2 <mehrad224090@gmail.com>
2026-09-21 11:47:51 +0330

    Declare default cached model runtime policy

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,1 +1,1 @@
-window.__KP_RUNTIME__={mode:"local",apiBase:"",release:"github-pages"};
+window.__KP_RUNTIME__={mode:"local",apiBase:"",release:"github-pages",modelSource:"auto",modelBase:"./models/"};
