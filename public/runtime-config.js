bbf91a36e62a4c24f199759a02a1f82218d2c252
Mehradt2 <mehrad224090@gmail.com>
2026-09-21 12:00:56 +0330

    Declare online model profile for browser release

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,1 +1,1 @@
-window.__KP_RUNTIME__={mode:"local",apiBase:"",release:"github-pages",modelSource:"auto",modelBase:"./models/"};
+window.__KP_RUNTIME__={mode:"local",apiBase:"",release:"github-pages",modelSource:"auto",modelBase:"./models/",modelProfile:"online"};
