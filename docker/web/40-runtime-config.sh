9b6cf7d11209992e7cc4ffac2e6b635f90f8134e
Mehradt2 <mehrad224090@gmail.com>
2026-09-21 11:47:54 +0330

    Expose Docker offline model source in runtime config

--- docker/web/40-runtime-config.sh
+++ docker/web/40-runtime-config.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -eu
+MODEL_SOURCE="${KP_MODEL_SOURCE:-auto}"
+case "$MODEL_SOURCE" in
+  auto|bundled) ;;
+  *) MODEL_SOURCE="auto" ;;
+esac
+MODEL_BASE="${KP_MODEL_BASE:-/models/}"
 cat > /usr/share/nginx/html/runtime-config.js <<EOF
-window.__KP_RUNTIME__={mode:"${KP_RUNTIME_MODE:-team}",apiBase:"",release:"${KP_RELEASE:-docker}"};
+window.__KP_RUNTIME__={mode:"${KP_RUNTIME_MODE:-team}",apiBase:"",release:"${KP_RELEASE:-docker}",modelSource:"$MODEL_SOURCE",modelBase:"$MODEL_BASE"};
 EOF
